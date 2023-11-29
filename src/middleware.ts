19d63344092be622cc0b5ed5716da103a7045ff7
Kittonn <tonkittipod@gmail.com>
2023-11-30 01:03:41 +0700

    refactor: refactor middlware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,30 +3,26 @@ import { NextRequest, NextResponse } from "next/server";
 const protectedRoutes = ["/dashboard"];
 
 export default async function middleware(req: NextRequest) {
-  const path = req.nextUrl.pathname;
+  const {pathname} = req.nextUrl;
   const session = !!req.cookies.get("next-auth.session-token");
 
-  if (path.startsWith("/s/")) {
-    const data = await fetch(`${req.nextUrl.origin}/api/url/${path.slice(3)}`);
+  if (pathname.startsWith("/s/")) {
+    const response = await fetch(`${req.nextUrl.origin}/api/url/${pathname.slice(3)}`);
 
-    if (data.status === 404) {
+    if (response.status === 404) {
       return NextResponse.redirect(new URL("/404", req.url));
     }
 
-    const { url } = await data.json();
-
-    if (!url) {
-      return NextResponse.redirect(new URL("/404", req.url));
-    }
+    const { url } = await response.json();
 
     return NextResponse.redirect(new URL(url));
   }
 
-  if (path.startsWith("/auth") && session) {
+  if (pathname.startsWith("/auth") && session) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
-  if (!session && protectedRoutes.includes(path)) {
+  if (!session && protectedRoutes.includes(pathname)) {
     return NextResponse.redirect(new URL("/auth", req.url));
   }
 }

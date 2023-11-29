ccdcdc4b80f1a8e9e901c9f2beab32539a1b8ce5
Kittonn <tonkittipod@gmail.com>
2023-11-29 17:30:07 +0700

    chore(create link): update create link from with react-hook-form

--- src/schema/link.ts
+++ src/schema/link.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 export const CreateLinkSchema = z.object({
   url: z.string().url(),
-  slug: z.string().min(1),
+  slug: z.string().min(1).max(6),
   description: z.string().optional(),
 });
-
